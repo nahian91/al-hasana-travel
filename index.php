ed9bf919c367f88fc9c97a05b04d2cc559ef962b
nahian28 <nahiansylhet@gmail.com>
2025-08-27 12:23:24 +0600

    Updates

--- index.php
+++ index.php
@@ -15,43 +15,91 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	 <!-- breadcrumb-wrappe-Section Start -->
+        <section class="breadcrumb-wrapper fix bg-cover" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/breadcrumb/breadcrumb.jpg);">
+            <div class="container">
+                <div class="row">
+                    <div class="page-heading">
+                        <h2>Blogs</h2>
+                        <ul class="breadcrumb-list">
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li><i class="fa-solid fa-chevrons-right"></i></li>
+                            <li>Blogs</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-		<?php
-		if ( have_posts() ) :
+		<section class="news-section section-padding fix">
+    <div class="container">
+        <div class="row g-4">
+            <?php
+            $args = array(
+                'post_type'      => 'post',
+                'posts_per_page' => 6, // Number of posts per page
+                'paged'          => get_query_var('paged') ? get_query_var('paged') : 1,
+            );
+            $loop = new WP_Query($args);
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+            if ($loop->have_posts()) :
+                while ($loop->have_posts()) : $loop->the_post();
+            ?>
+                <div class="col-xl-4 col-md-6 col-lg-6 wow fadeInUp" data-wow-delay=".3s">
+                    <div class="news-card-items-3 mt-0">
+                        <div class="news-image">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_post_thumbnail('medium', ['alt' => get_the_title()]); ?>
+                                </a>
+                            <?php endif; ?>
+                        </div>
+                        <div class="news-content">
+                            <ul class="post-meta">
+                                <li class="post"><?php echo get_the_date('d'); ?><span><?php echo get_the_date('M'); ?></span></li>
+                                <li>
+                                    <i class="fa-regular fa-user"></i>
+                                    By <?php the_author(); ?>
+                                </li>
+                                <li>
+                                    <i class="fa-regular fa-tag"></i>
+                                    <?php the_category(', '); ?>
+                                </li>
+                            </ul>
+                            <h4>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h4>
+							<?php the_excerpt();?>
+                            <a href="<?php the_permalink(); ?>" class="link-btn">Read More <i class="fa-sharp fa-regular fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+        <!-- Pagination -->
+        <div class="page-nav-wrap text-center">
+            <?php
+            echo paginate_links(array(
+                'total'   => $loop->max_num_pages,
+                'current' => max(1, get_query_var('paged')),
+                'prev_text' => '<i class="fal fa-long-arrow-left"></i>',
+                'next_text' => '<i class="fal fa-long-arrow-right"></i>',
+            ));
+            ?>
+        </div>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+        <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+            <p>No posts found.</p>
+        <?php endif; ?>
+    </div>
+</section>
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
